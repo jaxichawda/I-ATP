b81837f7c8fa261d5292f4b89b055c467d3fc470
uttambhut222 <39264925+uttambhut222@users.noreply.github.com>
2018-11-13 15:08:31 +0530

    ubc

---
+++ application/views/form.php
@@ -0,0 +1,49 @@
+<?php include("header.php"); ?>
+
+<!-- Inner Section -->
+<div class="inner_banner">
+    <img src="assets/images/inner_banner.jpg" alt="" class="img-responsive" />
+    <div class="innerbanner_text">HR QUESTIONNAIRE</div>
+</div>
+<div class="clearfix"></div>
+<!-- End Inner Section -->
+<!-- Inner Section -->
+<div class="inner_content">
+    <div class="container ">
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="clearfix"></div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="clearfix"></div>
+                        <form>
+                            <h1 class="text-center">Your Information</h1>
+                            <div class="separator text-center"><span class="dott"></span><span class="dott"></span><span
+                                    class="dott"></span></div>
+                            <div class="clearfix"></div>
+                            <div class="ans_block">
+                                <div class="col-md-2 col-sm-2">
+                                    <label class="pull-right mt-5">Your First Name</label>
+                                </div>
+                                <div class="col-md-4 col-sm-4">
+                                    <div class="form-group">
+                                        <input type="text" name="CandidateName" pattern="[a-zA-Z',&.$\ ]{2,100}"
+                                            maxlength="100" required />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-btn">
+                                    <button type="submit" class="lgn_btn margin_top"><span>Submit</span></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</div>
+<div class="clearfix"></div>
+<!-- End Inner Section -->
---
+++ application/views/header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>OpenEyes - HR Questionnaire</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  <link type="image/x-icon" rel="shortcut icon" href="assets/images/favicon.ico" />
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="assets/css/font-awesome.min.css" />
+  <link type="text/css" rel="stylesheet" href="assets/css/flaticon.css" />
+  <link type="text/css" rel="stylesheet" href="assets/css/style.css" />
+  <link type="text/css" rel="stylesheet" href="assets/css/jquery.dataTables.min.css" />
+  <link type="text/css" rel="stylesheet" href="assets/css/buttons.dataTables.min.css" />
+  <link type="text/css" rel="stylesheet" href="assets/css/responsive.dataTables.min.css" />
+  <link type="text/css" rel="stylesheet" href="assets/css/sweetalert2.min.css" />
+
+</head>
+
+<body>
+
+<!-- Header Section -->
+<header>
+	<div class="container">
+		<div class="row">
+			<!-- Logo -->
+			<div class="col-md-3 col-sm-2 col-xs-12">
+				<div class="logo"> <a href="http://theopeneyes.com/" target="_blank"><img src="assets/images/OSSlogo.png" alt="OpenEyes Software Solutions" title="OpenEyes Software Solutions" class="img-responsive tooltip_inline" data-placement="bottom" /></a> </div>
+			</div>
+			<!-- End Logo -->
+
+			<!-- Header Address -->
+			<div class="col-md-9 col-sm-10 col-xs-12">
+				<div class="header_address">
+					<div class="address"><i class="flaticon-placeholder" aria-hidden="true"></i><span><strong>Suite #405, Iscon Atria 1,</strong><br>Gotri Road, Vadodara 390021</span></div>
+					<div class="address"><i class="flaticon-smartphone" aria-hidden="true"></i><span><strong>Call Us Now</strong><br><a href="tel:997.881.3937">997.881.EYES (3937)</a></span></div>
+					<div class="address"><i class="flaticon-mail" aria-hidden="true"></i><span><strong>Send Us Email</strong><br><a href="mailto:hr@theopeneyes.com">hr@theopeneyes.com</a></span></div>
+				</div>
+			</div>
+			<!-- End Header Address -->
+		</div>
+		</div>
+	<div class="clearfix"></div>
+</header>
+<div class="clearfix"></div>
+<!-- End Header -->
