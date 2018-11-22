6b44078a78ae37163060d14416069f4e908bb72a
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2018-11-22 18:32:53 +0530

    ac

---
+++ logs/log-2018-11-14.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-14 11:34:55 --> 404 Page Not Found: InqueryList/index
+ERROR - 2018-11-14 11:35:12 --> 404 Page Not Found: InquieryList/index
+ERROR - 2018-11-14 11:36:10 --> 404 Page Not Found: Enquirylist/index
